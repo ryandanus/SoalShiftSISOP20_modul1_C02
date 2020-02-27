fb202d1c1ea355e2ca44bbc91b7e4d1558e47f43
FeinardSlim <58687783+FeinardSlim@users.noreply.github.com>
2020-02-27 19:24:42 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -30,7 +30,7 @@ END{
 echo "${jawaban1b[0]}"
 echo "${jawaban1b[1]}"
 
-awk -F '\t' -v x=${jawaban1b[0]} -v y={$jawaban1b[1]} -v z=$jawaban1a 'NR>1{ if($13 == z && ($11 == x || $11 == y)) array[$17]+=$21;}
+awk -F '\t' -v x=${jawaban1b[0]} -v y=${jawaban1b[1]} -v z=$jawaban1a 'NR>1{ if($13 == z && ($11 == x || $11 == y)) array[$17]+=$21;}
 END{
   for (i in array)
     print array[i] ":" i
